2ddb935ffef2637e6dea679950e82b4fc07551d5
jsie <julien.sie@gmail.com>
2016-06-30 10:49:08 +0200

    add final attribute

--- Misc/src/fr/yla/regex/EmailValidator.java
+++ Misc/src/fr/yla/regex/EmailValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class EmailValidator {
 
-	private Pattern pattern;
+	private final Pattern pattern;
 	private Matcher matcher;
 
 	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
