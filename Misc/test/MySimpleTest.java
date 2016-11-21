9f82391d387d4a0c31e63817a9f53bd1a115cea8
jsie <julien.sie@gmail.com>
2016-11-21 11:29:13 +0100

    Boolean test

--- Misc/test/MySimpleTest.java
+++ Misc/test/MySimpleTest.java
@@ -1,11 +1,15 @@
+
 import org.junit.Test;
 
 public class MySimpleTest {
 
-	@Test
-	public void testZ() {
-		int i = 1;
-		assert i == 1;
-	}
+    @Test
+    public void testZ() {
+        /*int i = 1;
+		assert i == 1;*/
+        System.out.println("false && false = " + (false && false));
+        assert false == (false && false);
+
+    }
 
 }
