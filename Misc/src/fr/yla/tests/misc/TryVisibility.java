0b9cee061283ae59fd3eaa7355ffa7b814c5463c
jsie <julien.sie@gmail.com>
2017-01-09 11:32:39 +0100

    try/catch scope

---
+++ Misc/src/fr/yla/tests/misc/TryVisibility.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.yla.tests.misc;
+
+/**
+ *
+ * @author jsie
+ */
+public class TryVisibility {
+
+    public static void main(String[] args) {
+        String message = "hello";
+
+        try {
+            message = provideMsg(false);
+        } catch (Exception ex) {
+            System.err.println("Exception... " + ex.getMessage());
+        } finally {
+            System.out.println("Message in finally = " + message);
+        }
+
+        System.out.println("Message out of the try/catch = " + message);
+    }
+
+    private static String provideMsg(boolean trigger) throws Exception {
+
+        if (trigger) {
+            throw new Exception("provideMsg() has thrown an exception");
+        }
+
+        return "bye";
+    }
+
+}
