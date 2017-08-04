b47b9faf6bd00ae23e67ec3bcd4ecd5a3e1d020c
jsie <julien.sie@gmail.com>
2017-08-04 15:48:50 +0200

    display JAVA_HOME each second

--- Misc/src/fr/yla/tests/misc/EnvVars.java
+++ Misc/src/fr/yla/tests/misc/EnvVars.java
@@ -1,11 +1,26 @@
 package fr.yla.tests.misc;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EnvVars {
 
-	public static void main(String[] args) {
-		System.getProperties().list(System.out);
-		
-		System.out.println("\nJAVA HOME = "+System.getProperty("java.home"));
-		System.out.println("JAVA VERSION = "+System.getProperty("java.version"));
-	}
+    public static void main(String[] args) {
+        //System.getProperties().list(System.out);
+        try {
+            while (true) {
+
+                System.out.println("\nJAVA HOME = " + System.
+                        getProperty("java.home"));
+                System.out.println("JAVA VERSION = " + System.
+                        getProperty("java.version"));
+
+                Thread.sleep(1000);
+
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(EnvVars.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+    }
 }
