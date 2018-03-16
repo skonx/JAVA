0349032deabac1e5c366781e9d1f847bcf6e90ce
jsie <julien.sie@gmail.com>
2018-03-16 22:21:36 +0100

    add tests to decimal test

--- Misc/src/fr/yla/tests/maths/TestBigDecimal.java
+++ Misc/src/fr/yla/tests/maths/TestBigDecimal.java
@@ -15,11 +15,24 @@ import java.math.RoundingMode;
 public class TestBigDecimal {
 
     public static void main(String[] args) {
-        int price = 1500;
+        int price = 1502;
         BigDecimal vtarate = new BigDecimal("5.5");
-        int netprice = (new BigDecimal(price).multiply(vtarate.add(
-                new BigDecimal(100)))).divide(new BigDecimal(100)).setScale(2,
-                RoundingMode.HALF_UP).intValue();
+        BigDecimal netprice = (new BigDecimal(price).multiply(vtarate.add(
+                new BigDecimal(100)))).divide(new BigDecimal(100)).setScale(0,
+                RoundingMode.HALF_UP);
         System.out.println(netprice);
+
+        BigDecimal bg1, bg2;
+
+        bg1 = new BigDecimal("123.12678");
+
+        // set scale of bg1 to 2 in bg2 using floor as rounding mode
+        bg2 = bg1.setScale(2, RoundingMode.HALF_UP);
+
+        String str = bg1 + " after changing the scale to 2 and rounding is "
+                + bg2;
+
+        // print bg2 value
+        System.out.println(str);
     }
 }
